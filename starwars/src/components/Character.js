c88db71d76c83a36068395db71efd2eec82443d9
unknown <nandhinimadan20@gmail.com>
2020-06-26 16:57:40 -0700

    Render Completed

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,25 @@
 // Write your Character component here
+import React,{useEffect,useState} from "react";
+import axios from "axios";
+import CharacterList from "./CharacterList";
+
+export default function Character(){
+    const [Characters,SetCharacter]=useState([]);
+    useEffect(()=>{
+    axios
+    .get('https://swapi.dev/api/people/1/')
+    .then(res=>{
+        console.log("Starwars",res);
+        SetCharacter(res.data);
+    })
+    .catch(err=>{
+        console.log("Error Occured",err);
+    });
+},[]);
+
+    return(<div>
+        <CharacterList key={Characters.id} Name={Characters.name}/>
+    </div>)
+
+}
+
---
+++ starwars/src/components/CharacterList.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const CharacterList=({name})=>{
+    return(
+        <div>
+            <h1>Name:{name}</h1>
+        </div>
+    )
+}
+export default CharacterList;
