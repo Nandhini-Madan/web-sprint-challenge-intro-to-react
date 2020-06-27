61db8175061e2364f699629b554b4937e0e9debc
unknown <nandhinimadan20@gmail.com>
2020-06-26 18:45:10 -0700

    Styled using Styled components

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -5,20 +5,33 @@ import CharacterList from "./CharacterList";
 
 export default function Character(){
     const [Characters,SetCharacter]=useState([]);
+   const number=1;
+   
     useEffect(()=>{
-    axios
-    .get('https://swapi.dev/api/people/1/')
-    .then(res=>{
-        console.log("Starwars",res);
-        SetCharacter(res.data);
-    })
-    .catch(err=>{
-        console.log("Error Occured",err);
-    });
+      // number.forEach(num=>{
+        axios
+        .get(`https://swapi.dev/api/people/${number}/`)
+        .then(res=>{
+            console.log("Starwars",res);
+            SetCharacter(res.data);
+        })
+        .catch(err=>{
+            console.log("Error Occured",err);
+        });
+     //  })
+   
+       
 },[]);
+    
 
     return(<div>
-        <CharacterList key={Characters.id} Name={Characters.name}/>
+
+        <CharacterList key={Characters.id} Name={Characters.name} url={Characters.url} mass={Characters.mass} films={Characters.films}
+
+        Gender={Characters.gender}
+         />
+    
+    
     </div>)
 
 }
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-const CharacterList=({name})=>{
+//styled components
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: blue`;
+const Pag=styled.p`
+font-size: 1.0em;
+text-align: center;
+color: orange`;
+
+const CharacterList=({Name,url,mass,films,Gender})=>{
+    //console.log(films);
+
     return(
         <div>
-            <h1>Name:{name}</h1>
+            <Title>Name: {Name}</Title>
+            <Pag>Mass: {mass}</Pag>
+            <p>URL: {url}</p>
+            <p>Gender:{Gender} </p>
+            
         </div>
     )
 }
