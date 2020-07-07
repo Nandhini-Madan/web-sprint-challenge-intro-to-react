3cdc817af0ee2e635815047a95a93aad9243adaf
unknown <nandhinimadan20@gmail.com>
2020-07-06 19:18:56 -0700

    Completed sprint success

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -2,25 +2,25 @@
 import React,{useEffect,useState} from "react";
 import axios from "axios";
 import CharacterList from "./CharacterList";
-import styled from "styled-components";
+
 
 export default function Character(){
     const [Characters,SetCharacter]=useState([]);
    const number=[1,2,3];
    
     useEffect(()=>{
-       number.forEach(num=>{
+      
         axios
-        .get(`https://swapi.dev/api/people/${num}/`)
+        .get(`https://swapi.dev/api/people/`)
         .then(res=>{
             console.log("Starwars",res);
-            SetCharacter([...Characters, res.data]);
+            SetCharacter( res.data.results);
             
         })
         .catch(err=>{
             console.log("Error Occured",err);
         });
-       })
+    
    
        
 },[]);
@@ -29,15 +29,17 @@ export default function Character(){
     return(
     <div>
         {console.log("Character",Characters)}
-        {Characters.map(p=>(
+     {Characters.map(chara=>{
+         return(
+            
+        <CharacterList key={chara.id} Name={chara.name} url={chara.url} mass={chara.mass} films={chara.films}
 
-        <CharacterList key={p.id} Name={p.name} url={p.url} mass={p.mass} films={p.films}
+        Gender={chara.gender} />
+         );
 
-        Gender={p.gender}
-         />
-         ) )}
+     })}
     
-    </div>)
+    </div>);
 
 }
 
--- starwars/src/components/CharacterList.js
+++ starwars/src/components/CharacterList.js
@@ -7,27 +7,40 @@ const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   color: blue`;
-  
+
 const Pag=styled.p`
 font-size: 1.0em;
 text-align: center;
-color: orange`;
+color: green`;
+
+const Film=styled.p`
+font-size:1.0em;
+color:red`;
+
+const Border=styled.div`
+border: 4px dotted blue`;
+
+
+
 
 const CharacterList=({Name,url,mass,films,Gender})=>{
-    //console.log(films);
+    console.log("Films",films);
+    console.log("Name1",Name);
 
     return(
-        <div>
+        
+        <Border>
             <Title>Name: {Name}</Title>
             <Pag>Mass: {mass}</Pag>
-            <p>URL: {url}</p>
+            <Pag>URL: {url}</Pag>
             <p>Gender:{Gender} </p>
+            <Film>Films</Film>
             {films.map(film=>(
                 <p>{film}</p>
             ))}
          
             
-        </div>
+        </Border>
     )
 }
 export default CharacterList;
